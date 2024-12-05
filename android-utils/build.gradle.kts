f8eb51651b1fe172d340c73d3a543b70d75421ad
Dominik Moog <Dominik_moog@hotmail.de>
2024-12-05 16:02:02 +0100

    update agp and libraries

--- android-utils/build.gradle.kts
+++ android-utils/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.dom.android_utils"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         minSdk = 24
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = JavaVersion.VERSION_21.toString()
     }
     tasks.withType<Test> {
         useJUnitPlatform()
